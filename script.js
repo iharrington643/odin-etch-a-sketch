97ed927e7c0978b21a8d898fa069511fdfafd700
Isabella Harrington <isabellaharrington643@gmail.com>
2025-06-24 21:00:43 -0400

    Added color buttons to JavaScript file
    Added event listeners to change paint color upon click

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
 const container = document.getElementById("grid-container");
 const buttonBox = document.getElementById("button-box");
+const colorPalette = document.getElementById("color-palette");
 
 const newGridButton = document.createElement('button');
 newGridButton.classList.add('new-grid-button');
 newGridButton.textContent = "Create New Grid";
 buttonBox.appendChild(newGridButton);
 
+const redButton = document.createElement('button');
+redButton.classList.add('red-button');
+colorPalette.appendChild(redButton);
+
+const yellowButton = document.createElement('button');
+yellowButton.classList.add('yellow-button');
+colorPalette.appendChild(yellowButton);
+
+const blueButton = document.createElement('button');
+blueButton.classList.add('blue-button');
+colorPalette.appendChild(blueButton);
+
+const blackButton = document.createElement('button');
+blackButton.classList.add('black-button');
+colorPalette.appendChild(blackButton);
+
+const eraseButton = document.createElement('button');
+eraseButton.classList.add('erase-button');
+eraseButton.textContent = "Erase";
+colorPalette.appendChild(eraseButton);
+
 let gridSize = 16;
+let paintColor = 'black';
 
 createGrid(gridSize);
+
+redButton.addEventListener('click', () => {
+    paintColor = 'red';
+});
+
+yellowButton.addEventListener('click', () => {
+    paintColor = 'yellow';
+});
+
+blueButton.addEventListener('click', () => {
+    paintColor = 'blue';
+});
+
+blackButton.addEventListener('click', () => {
+    paintColor = 'black';
+});
+
+eraseButton.addEventListener('click', () => {
+    paintColor = 'white';
+});
 
 newGridButton.addEventListener('click', () => {
     let newSize = parseInt(prompt("How many squares per side? (Maximum of 100)"))
@@ -36,7 +79,8 @@ function createGrid(size) {
         gridSquares.style.height = `${squareSize}px`;
 
         gridSquares.addEventListener('mouseover', () => {
-            gridSquares.style.backgroundColor = 'black';
+            gridSquares.style.backgroundColor = paintColor;
         });
     }
 }
+
